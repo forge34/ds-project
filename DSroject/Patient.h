66e460c3535607c63678f01ca8afec9e67e42619
Mohamed Abdeltawab <mohamedabdeltawab338@gmail.com>
2024-12-01 23:25:51 +0200

    add patient class

--- DSroject/Patient.h
+++ DSroject/Patient.h
@@ -1,13 +1,47 @@
 #pragma once
 
 #include <iostream>
+using namespace std;
 
 class Patient
 {
-public:
-	void hello();
-};
+private:
+	int id;
+	string name;
+	string address;
+	string appointment;
 
-void Patient::hello() {
-	std::cout << " Hello";
+public:
+	void set_id(int);
+	void set_name(string);
+	void set_address(string);
+	void set_appointment(string);
+	int get_id();
+	string get_name();
+	string get_address();
+	string get_appointment();
+};
+void Patient::set_id(int id) {
+	this->id = id;
+}
+void Patient::set_name(string name) {
+	this->name = name;
+}
+void Patient::set_address(string address) {
+	this->address = address;
+}
+void Patient::set_appointment(string appointment) {
+	this->appointment = appointment;
+}
+int Patient::get_id() {
+	return id;
+}
+string Patient::get_name() {
+	return name;
+}
+string Patient::get_address() {
+	return address;
+}
+string Patient::get_appointment() {
+	return appointment;
 }
